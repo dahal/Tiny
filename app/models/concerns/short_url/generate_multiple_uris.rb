25ea1c3da0f44a77c92a274ce2f8d660d0443f3e
Puru Dahal <pdahal@me.com>
2015-07-19 20:01:21 -0700

    Refactor uri generate!

--- app/models/concerns/short_url/generate_multiple_uris.rb
+++ app/models/concerns/short_url/generate_multiple_uris.rb
@@ -4,7 +4,16 @@ class ShortUrl
 
     module ClassMethods
       def generate!(long_url_id)
-        self.create(uri: SecureRandom.urlsafe_base64(4), long_url_id: long_url_id)
+        self.create(uri: generate_new_uri, long_url_id: long_url_id)
+      end
+
+      private
+
+      def generate_new_uri
+        loop do
+          uri = SecureRandom.urlsafe_base64(4)
+          return uri if !ShortUrl.exists?(uri: uri)
+        end
       end
     end
   end
