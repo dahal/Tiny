f5b034a1c162339c5b0a14bd09849ce6c9be6203
Puru Dahal <pdahal@me.com>
2015-07-25 22:55:02 -0700

    Add simple form

--- app/controllers/long_urls_controller.rb
+++ app/controllers/long_urls_controller.rb
@@ -1,9 +1,17 @@
 class LongUrlsController < ApplicationController
+  before_action :load_new_url, only: [:index, :new]
+  
   def index
     @urls = LongUrl.all
   end
   
   def create
+    require "pry"; binding.pry
     redirect_to :long_urls
+  end
+  
+  private
+  def load_new_url
+    @url = LongUrl.new
   end
 end
