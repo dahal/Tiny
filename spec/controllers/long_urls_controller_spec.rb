510f27094c694bb54f2048f351a3cf275f159c32
Puru Dahal <pdahal@me.com>
2015-07-26 02:55:54 -0700

    Fix controller#create specs

--- spec/controllers/long_urls_controller_spec.rb
+++ spec/controllers/long_urls_controller_spec.rb
@@ -9,10 +9,20 @@ describe LongUrlsController do
   end
 
   context '#create' do
-    subject { post :create, url: FactoryGirl.create(:long_url) }
+    context "when the long_url is a valid url" do
+      subject {post :create, {long_url: {url: 'http://google.com'}}}
+      
+      it { expect {subject}.to change(LongUrl, :count).by(1) }
+      it { expect {subject}.to change(ShortUrl, :count) }
+      it { expect(subject).to redirect_to :root }
+    end
     
-    it { expect {subject}.to change(LongUrl, :count).by(1) }
-    it { expect {subject}.to change(ShortUrl, :count) }
-    it { expect(subject).to redirect_to :long_urls }
+    context "when the long_url is an invalid url" do
+      subject {post :create, {long_url: {url: 'google.com'}}}
+    
+      it { expect {subject}.to_not change(LongUrl, :count) }
+      it { expect {subject}.to_not change(ShortUrl, :count) }
+      it { expect(subject).to render_template :new }
+    end
   end
 end
